bf1350b7f80eb269b8ea21f37804a6c31a5f513f
Matthew Taing <taingmat@uw.edu>
2020-03-09 13:24:32 -0700

    added grid drawing feature

--- HelloWorldMP5/public_html/src/MyGame/Objects/Player.js
+++ HelloWorldMP5/public_html/src/MyGame/Objects/Player.js
@@ -6,6 +6,7 @@ function Player()
     this.square = new Renderable();
     this.square.setColor([1, 0, 0, 1]);
     this.path = [];
+    this.pathLines = []; 
     GameObject.call(this, this.square);
 }
 
@@ -17,6 +18,22 @@ Player.prototype.update = function(mGrid)
     {
         var result = mGrid.search(this.getXform().getPosition(), [50, 80]);
         this.path = result;
+        
+        var i = 0; 
+        for(i = 0; i < this.path.length - 2; i++) {
+            var startNode = this.path[i];
+//            console.log(startNode);
+//            console.log(startNode.x); 
+//            console.log(startNode.y);
+            var endNode = this.path[i + 1];
+//            console.log(endNode.x); 
+//            console.log(endNode.y);
+            var tempLine = new LineRenderable(startNode.x, 
+                    startNode.y, 
+                    endNode.x, 
+                    endNode.y);
+            this.pathLines.push(tempLine); 
+        }  
     }
     
     if(this.path !== null && this.path.length > 0)
@@ -29,4 +46,14 @@ Player.prototype.update = function(mGrid)
 Player.prototype._arrived = function()
 {
     return (this.path.length === 0 );
+};
+
+Player.prototype.draw = function(mCamera) 
+{
+  this.square.draw(mCamera)
+//  console.log(this.pathLines.length);
+  var i = 0; 
+  for (i = 0; i < this.pathLines.length; i++) {
+      this.pathLines[i].draw(mCamera);
+  }
 };
