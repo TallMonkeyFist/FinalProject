da4102a549aeccfd755ef2dc1e81763083b177a9
Matthew Taing <taingmat@uw.edu>
2020-03-16 12:29:03 -0700

    made it so that players can follow sets of waypoints

--- HelloWorldMP5/public_html/src/MyGame/Objects/Player.js
+++ HelloWorldMP5/public_html/src/MyGame/Objects/Player.js
@@ -13,7 +13,7 @@ function Player(mGrid)
     this.currPathIndex = 0; 
     this.followWayPoints = true;
     this.mPath.setGrid(mGrid);
-    this.mPath.setSpeed(150);
+    this.mPath.setSpeed(350);
     this.mSpeed = 5;
 }
 
@@ -53,6 +53,19 @@ Player.prototype.update = function(mGrid, camera)
 //            this.mPath.findPath(this.getXform().getPosition(), [destination[0], destination[1]]);
 //        }
 //    }
+
+    if (this.followWayPoints) 
+    {
+        if (this.mPath.path === null || this.mPath.path.length === 1)  
+        {
+            this.mPath.findMultiPath(this.getXform().getPosition(), this.wayPoints);
+//            this.mPath.findPath(this.getXform().getPosition(), this.wayPoints[this.currPathIndex]);
+//            this.currPathIndex = this.currPathIndex + 1; 
+//            if (this.currPathIndex >= this.wayPoints.length) {
+//                this.currPathIndex = 0; 
+//            }
+        }
+    }
     
     this.mPath.update(camera, this.mSpeed);
 };
